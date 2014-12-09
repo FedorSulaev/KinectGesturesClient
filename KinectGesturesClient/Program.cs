9725498bc7fad6ea91fb3b6eafc0e704cb005883
FedorSulaev <fedor.sulaev@outlook.com>
2014-12-09 11:01:11 +0300

    Initial

---
+++ KinectGesturesClient/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using KinectGestures;
+
+namespace KinectGesturesClient
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			KinectService service = new KinectService();
+			Console.WriteLine(service.GetKinectStatus().ToString());
+			if (service.GetKinectStatus() == KinectStatus.Connected)
+			{
+				service.StartKinect();
+				service.GestureEvent += service_GestureEvent;
+			}
+			while (true)
+			{
+				
+			}
+		}
+
+		static void service_GestureEvent(object sender, KinectGesture e)
+		{
+			Console.WriteLine(e.ToString());
+		}
+	}
+}
